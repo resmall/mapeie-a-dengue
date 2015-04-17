5a43a3b9485379581a22d151e8b75efe308386ab
Tiago R. Lammers <trlammers@gmail.com>
2015-04-17 19:18:46 +0000

    initial

---
+++ public/src/saveLocalization.php
@@ -0,0 +1,10 @@
+<?php 
+echo "Informações recebidas:".  $_POST['name'] ." e ". $_POST['time'];
+
+// BD: id|username|lng|lat|timestamp
+
+
+// verifica se a pessoa já marcou 3 spots
+// se marcou não pode marcar mais
+// se não marcou, salva e retorna confirmação
+
