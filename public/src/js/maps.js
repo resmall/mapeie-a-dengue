c2638e1520e24bd207bc14ce250b19dc4c258600
Tiago R. Lammers <trlammers@gmail.com>
2015-04-24 03:13:48 +0000

    Agrupando os pontos no mapa com base na proximidade

--- public/src/js/maps.js
+++ public/src/js/maps.js
@@ -1,23 +1,46 @@
+var markermap = function (markers) {
+
+    return function(data) {
+        var obj = data; //JSON.parse(data);
+        for(var i = 0; i < obj.length; i++) {
+            var position = new google.maps.LatLng(obj[i].lat, obj[i].lng); //3 param bool
+
+            //placeMarker(position, map);
+            var marker = new google.maps.Marker({'position': position});
+            markers.push(marker);
+            console.log('Markcadores1' + markers[0]);
+        }
+    };
+};
+
+
 function initialize() {
     var mapOptions = {
-        zoom: 18,
+        zoom: 15,
         center: new google.maps.LatLng(-28.2898836,-53.4998947)
     };
 
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    var markers = [];
+    var markerCluster;
 
+    // recupera os marcadores e adiciona no mapa
     $.post("src/getMarkers.php")
     .done(function(data) {
         var obj = data; //JSON.parse(data);
         for(var i = 0; i < obj.length; i++) {
             var position = new google.maps.LatLng(obj[i].lat, obj[i].lng); //3 param bool
-            placeMarker(position, map);
+            //placeMarker(position, map);
+            var marker = new google.maps.Marker({'position': position});
+            markers.push(marker);
         }
+        markerCluster = new MarkerClusterer(map, markers);
     })
     .fail(function() {
         alert("error");
     });
 
+    // adiciona o listenter do click no map-canvas
     google.maps.event.addListener(map, 'click', function(e) {
         // 1 - Verifica se o usuário está logado, se não, aborta tudo e pede pra logar
         // 2 - Se logado, tenta salvar no banco de dados e verifica se já 
@@ -40,7 +63,11 @@ function initialize() {
 
                         // independente da mensagem que for exibida, se deu certo, marcamos o mapa
                         if(data.status == 'success') {
-                            placeMarker(e.latLng, map);
+                            //placeMarker(e.latLng, map);
+                            markerCluster.addMarker(new google.maps.Marker({
+                                position: e.latLng,
+                                map: map
+                            }));
                         }
                     })
                     .fail(function() {
@@ -52,22 +79,10 @@ function initialize() {
         });
     });
 }
-            
-function placeMarker(position, map) {
-    var marker = new google.maps.Marker({
-        position: position,
-        map: map
-    });
-    map.panTo(position);
-}
 
 function setModal(title, message) {
     $("#myModal .modal-title").html(title);
     $("#myModal .modal-body").html(message);
-}
-
-function loadExistingMarkers() {
-
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
